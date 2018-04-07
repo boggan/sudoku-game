ea4a311df54a976c2b173c76ee11541134c8ccf3
boggan <boggan@gmail.com>
2018-04-07 00:35:58 -0400

    optimized components to re-use existing code

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,15 +1,14 @@
 <template>
     <div class="play-sheet">
         <div class="sudoku-board">
-            <Cluster class="top c1" :numbers="cluster1" :solution="solution1" :showSolution="showSolution" />
-            <Cluster class="top c2" :numbers="cluster2" :solution="solution2" :showSolution="showSolution" />
-            <Cluster class="top c3" :numbers="cluster3" :solution="solution3" :showSolution="showSolution" />
-            <Cluster class="middle c1" :numbers="cluster4" :solution="solution4" :showSolution="showSolution" />
-            <Cluster class="middle c2" :numbers="cluster5" :solution="solution5" :showSolution="showSolution" />
-            <Cluster class="middle c3" :numbers="cluster6" :solution="solution6" :showSolution="showSolution" />
-            <Cluster class="bottom c1" :numbers="cluster7" :solution="solution7" :showSolution="showSolution" />
-            <Cluster class="bottom c2" :numbers="cluster8" :solution="solution8" :showSolution="showSolution" />
-            <Cluster class="bottom c3" :numbers="cluster9" :solution="solution9" :showSolution="showSolution" />
+            <!-- or v-for n in 9 // 1 -> 9 would build c1 -> c9 -->
+            <Cluster v-for="idx in 9"
+                     v-bind:key="idx"
+                     :class="'c' + idx"
+                     :clusterIdx="idx - 1"
+                     :sheet="sheet" 
+                     :solution="solution" 
+                     :showSolution="showSolution" />
         </div>
         <div class="signature">{{signature}}</div> 
     </div>
@@ -19,95 +18,10 @@
 import Cluster from "./Cluster";
 
 //=============================================================================
-function _extractClusterNumber(i_aBoard, i_nStartRow, i_nColumnOffset) {
-  let l_nEndColumn = i_nColumnOffset + 3;
-
-  return i_aBoard[i_nStartRow]
-    .slice(i_nColumnOffset, l_nEndColumn)
-    .concat(i_aBoard[i_nStartRow + 1].slice(i_nColumnOffset, l_nEndColumn))
-    .concat(i_aBoard[i_nStartRow + 2].slice(i_nColumnOffset, l_nEndColumn));
-}
-
-//=============================================================================
 export default {
   name: "Board",
   components: { Cluster },
-  props: ["sheet", "solution", "signature", "showSolution"],
-
-  // need to find a way to generate this...
-  computed: {
-    cluster1() {
-      return _extractClusterNumber(this.sheet, 0, 0);
-    },
-
-    solution1() {
-      return _extractClusterNumber(this.solution, 0, 0);
-    },
-
-    cluster2() {
-      return _extractClusterNumber(this.sheet, 0, 3);
-    },
-
-    solution2() {
-      return _extractClusterNumber(this.solution, 0, 0);
-    },
-
-    cluster3() {
-      return _extractClusterNumber(this.sheet, 0, 6);
-    },
-
-    solution3() {
-      return _extractClusterNumber(this.solution, 0, 0);
-    },
-
-    cluster4() {
-      return _extractClusterNumber(this.sheet, 3, 0);
-    },
-
-    solution4() {
-      return _extractClusterNumber(this.solution, 0, 0);
-    },
-
-    cluster5() {
-      return _extractClusterNumber(this.sheet, 3, 3);
-    },
-
-    solution5() {
-      return _extractClusterNumber(this.solution, 0, 0);
-    },
-
-    cluster6() {
-      return _extractClusterNumber(this.sheet, 3, 6);
-    },
-
-    solution6() {
-      return _extractClusterNumber(this.solution, 0, 0);
-    },
-
-    cluster7() {
-      return _extractClusterNumber(this.sheet, 6, 0);
-    },
-
-    solution7() {
-      return _extractClusterNumber(this.solution, 0, 0);
-    },
-
-    cluster8() {
-      return _extractClusterNumber(this.sheet, 6, 3);
-    },
-
-    solution8() {
-      return _extractClusterNumber(this.solution, 0, 0);
-    },
-
-    cluster9() {
-      return _extractClusterNumber(this.sheet, 6, 6);
-    },
-
-    solution9() {
-      return _extractClusterNumber(this.solution, 0, 0);
-    },
-  }
+  props: ["sheet", "solution", "signature", "showSolution"]
 };
 </script>
 
@@ -118,29 +32,41 @@ export default {
   height: 360px;
 }
 
-.c1 {
+.c1,
+.c4,
+.c7 {
   left: 0;
 }
 
-.c2 {
+.c2,
+.c5,
+.c8 {
   left: 120px;
 }
 
-.c3 {
+.c3,
+.c6,
+.c9 {
   left: 240px;
 }
 
-.top {
+.c1,
+.c2,
+.c3 {
   border: 1px SOLID #d3d5d5;
   top: 0;
 }
 
-.middle {
+.c4,
+.c5,
+.c6 {
   border: 1px SOLID #00ff00;
   top: 120px;
 }
 
-.bottom {
+.c7,
+.c8,
+.c9 {
   border: 1px SOLID #0000ff;
   top: 240px;
 }
--- src/components/Cluster.vue
+++ src/components/Cluster.vue
@@ -1,72 +1,63 @@
 <template>
     <div class="cluster">
-        <div>
-            <span v-if="showSolution" class="solution-cell">{{solution[0]}}</span>
-            <span v-else class="play-cell">{{numbers[0]}}</span>
-        </div>
-        <div>
-            <span v-if="showSolution" class="solution-cell">{{solution[1]}}</span>
-            <span v-else class="play-cell">{{numbers[1]}}</span>
-        </div>
-        <div>
-            <span v-if="showSolution" class="solution-cell">{{solution[2]}}</span>
-            <span v-else class="play-cell">{{numbers[2]}}</span>
-        </div>
-        
-        <div>
-            <span v-if="showSolution" class="solution-cell">{{solution[3]}}</span>
-            <span v-else class="play-cell">{{numbers[3]}}</span>
-        </div>
-        <div>
-            <span v-if="showSolution" class="solution-cell">{{solution[4]}}</span>
-            <span v-else class="play-cell">{{numbers[4]}}</span>
-        </div>
-        <div>
-            <span v-if="showSolution" class="solution-cell">{{solution[5]}}</span>
-            <span v-else class="play-cell">{{numbers[5]}}</span>
-        </div>
-
-        <div>
-            <span v-if="showSolution" class="solution-cell">{{solution[6]}}</span>
-            <span v-else class="play-cell">{{numbers[6]}}</span>
-        </div>
-        <div>
-            <span v-if="showSolution" class="solution-cell">{{solution[7]}}</span>
-            <span v-else class="play-cell">{{numbers[7]}}</span>
-        </div>
-        <div>
-            <span v-if="showSolution" class="solution-cell">{{solution[8]}}</span>
-            <span v-else class="play-cell">{{numbers[8]}}</span>
+        <div v-for="(number, idx) in cluster" 
+             v-bind:key="number + ':' + idx" 
+             v-bind:class="{ 'solution-cell': String(number).trim() === '' }" >
+            <span v-if="showSolution">{{cSolution[idx]}}</span>
+            <span v-else class="play-cell">{{number}}</span>
         </div>
     </div>
 </template>
 
 <script>
+//=============================================================================
+function _extractClusterNumber(i_aSheet, i_nStartRow, i_nColumnOffset) {
+  let l_nEndColumn = i_nColumnOffset + 3;
+
+  return i_aSheet[i_nStartRow]
+    .slice(i_nColumnOffset, l_nEndColumn)
+    .concat(i_aSheet[i_nStartRow + 1].slice(i_nColumnOffset, l_nEndColumn))
+    .concat(i_aSheet[i_nStartRow + 2].slice(i_nColumnOffset, l_nEndColumn));
+}
+
+//=============================================================================
 export default {
   name: "Cluster",
-  props: ["showSolution", "numbers", "solution"]
-}
+  props: ["clusterIdx", "showSolution", "sheet", "solution"],
+  data: function() {
+      let l_nRest = this.clusterIdx % 3,
+        l_nStartRow = Math.abs(Math.floor(l_nRest) - this.clusterIdx),
+        l_nColumnOffset = l_nRest * 3;
+
+      return {
+          cluster: _extractClusterNumber(this.sheet, l_nStartRow, l_nColumnOffset),
+          cSolution:  _extractClusterNumber(this.solution, l_nStartRow, l_nColumnOffset)
+      };
+  }
+};
 </script>
 
 <style scoped>
 .cluster {
-    position: absolute;
-    margin: 10px;
-    width: 90px;
-    height: 90px;
-    display: flex;
-    flex-flow: row;
-    flex-wrap: wrap;
+  position: absolute;
+  margin: 10px;
+  width: 96px;
+  height: 96px;
+  display: flex;
+  flex-flow: row;
+  flex-wrap: wrap;
 }
 
 .cluster div {
-    width: 30px;
-    text-align: center;
-    padding-top: 6px;
+  width: 30px;
+  min-height: 24px;
+  text-align: center;
+  padding-top: 6px;
+  border: 1px solid brown;
 }
 
 .solution-cell {
-    display: none;
+  background-color: #d3d3d3;
 }
 </style>
 
